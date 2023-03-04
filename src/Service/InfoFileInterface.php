5dc1b7f7a5b833a2dde407f541e56dd6523cff56
thomas <thomasthomasz2xz@gmail.com>
2023-03-04 18:59:16 +0100

    Feat(Service): ajout de du service PathInterface et du service InfoFileInterface

---
+++ src/Service/InfoFileInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Timetable\Timetable;
+use App\Kernel;
+use App\Lib\JsonFile;
+
+class InfoFileInterface
+{
+    private PathInterface $path;
+
+    public function __construct(PathInterface $path)
+    {
+        $this->path = $path;
+    }
+
+    public function getTimetable(): Timetable|false
+    {
+        if (!($file = JsonFile::ConstructWithPath($this->path->getInfoFilePath())))
+            return false;
+        elseif (!($arr = $file->getInFile(Timetable::KEY_TIMETABLE)))
+            return false;
+        elseif (!is_array($arr))
+            return false;
+        return Timetable::ConstructWithArray($arr);
+    }
+
+    public function setTimetable(Timetable $timetable)
+    {
+        $json = JsonFile::ConstructWithPath($this->path->getInfoFilePath());
+        if (!$json)
+            return;
+        #TODO: FINIR CETTE FONCTION
+
+    }
+
+}
