ace02f714ed133568f6be13784b9d759b9880ccc
thomas <thomasthomasz2xz@gmail.com>
2023-03-07 09:27:48 +0100

    feat(header, footer): Ajout du template et de css pour le header et le footer

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+require('bootstrap-icons/font/bootstrap-icons.css');
+import 'bootstrap'
 import './styles/app.scss';
 
 // start the Stimulus application
