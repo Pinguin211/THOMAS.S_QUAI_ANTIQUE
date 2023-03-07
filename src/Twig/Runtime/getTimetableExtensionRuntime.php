a48277f718c7384c8b4681c9186d86c993081bfd
thomas <thomasthomasz2xz@gmail.com>
2023-03-07 09:32:14 +0100

    Feat(Twig): ajout d'une fonction twig qui permet de recuperer l'objet pour les horaires en twig

---
+++ src/Twig/Runtime/getTimetableExtensionRuntime.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Twig\Runtime;
+
+use App\Entity\Timetable\Timetable;
+use App\Service\InfoFileInterface;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class getTimetableExtensionRuntime implements RuntimeExtensionInterface
+{
+    private InfoFileInterface $infoFile;
+
+    public function __construct(InfoFileInterface $infoFile)
+    {
+        $this->infoFile = $infoFile;
+    }
+
+    public function getTimetable(): Timetable | false
+    {
+        return $this->infoFile->getTimetable();
+    }
+}
